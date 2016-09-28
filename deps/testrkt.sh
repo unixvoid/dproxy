5e0d88b72a5c1ec143297afd4b97b464977a0ac9
Matthew Faltys <mfaltys@gmail.com>
2016-09-27 20:08:01 -0500

    Add systemd service

--- deps/testrkt.sh
+++ deps/testrkt.sh
@@ -3,12 +3,13 @@ CURRENT_PATH=$(pwd)
 
 sudo rkt run \
 	--insecure-options=all \
-	--net=host \
         --volume redis,kind=host,source=/tmp/ \
         --volume config,kind=host,source=$CURRENT_PATH/config.gcfg \
         --volume upstream,kind=host,source=$CURRENT_PATH/upstream/ \
+	--port=dns-tcp:53 \
+	--port=dns-udp:53 \
 	--debug \
-        ./dproxy.aci
+        unixvoid.com/dproxy
 
 #CURRENT_DIR=$(pwd)
 #--port=dns-tcp:8053 \
